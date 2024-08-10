a0f7ae42e1f872800d828073bf3aa5f174943723
sebastianSanchez05 <sebastiansanchezst@gmail.com>
2024-08-10 15:50:46 -0500

    Se agrego la primera ruta

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
+
+app(routes);
 
 app.listen(3005, () => console.log('Servidor corriendo http://localhost:3005'));
